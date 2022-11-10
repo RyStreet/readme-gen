886d3476b4ee93ba275c915171e1b881927ae08d
Ryan Street <ryanstreet122@gmail.com>
2022-11-10 14:03:29 -0500

    README generator function working

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,94 @@
 // TODO: Include packages needed for this application
+const fs = require('fs')
+const inquirer = require('inquirer');
+const { default: Choices } = require('inquirer/lib/objects/choices');
+const generateMarkdown = require('./utils/generateMarkdown');
+const markDown = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'user',
+        message: 'What is your GitHub username?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?'
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is your project description?'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Describe your Installation instructions:'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: "Describe your project's usage:",
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: "Describe your project's contribution guidelines:",
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: "Describe your project's tests:",
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose your project license',
+        choices: ['Academic Free License v3.0','Apache license 2.0','Artistic license 2.0','Boost Software License 1.0','Creative Commons license family','Do What The F*ck You Want To Public License','Educational Community License v2.0','European Union Public License 1.1','GNU General Public License family','Microsoft Public License','MIT','University of Illinois/NCSA Open Source License','zLib License']
+    }
+];
+
+function writeFile(){
+    return inquirer.prompt(questions)
+    .then((answers) => {
+    const mark = generateMarkdown(answers)
+        fs.writeFile('README.md', mark, err => {
+        if (err){
+            console.log('Error', err)
+        }
+        else {
+            console.log("Generating README!")
+        }
+    } )
+})
+    .catch((err) => {
+        console.log(err);
+
+})
+}
+
+writeFile()
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+// function writeToFile(fileName, data) {
+//     fs.writeFile(fileName, data, err => {
+//         if(err){
+//             console.log(err);
+//         }
+//         console.log('Creating your README file!')
+//     });
+// }
 
-// Function call to initialize app
-init();
+// // TODO: Create a function to initialize app
+// function init() {}
+
+// // Function call to initialize app
+// init();
--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# readme-gen
+# README Generator
+
+ ## Table of Contents
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contribution](#contribution)
+  - [Tests](#tests)
+
+  ## Description
+  Welcome to the README Generator! This project is a command-line app that utilizes node.js and inquierer to generate professional READMEs.
+
+  ## Installation
+  NA
+
+  ## Usage
+  Generating READMEs
+
+  ## Contribution
+  NA
+
+  ## Tests
+  NA
+
+  ## License
+  MIT
